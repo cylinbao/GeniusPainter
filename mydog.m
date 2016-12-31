8156eedef491973a88e8470c60f81658226cd4c3
Chien Yu Lin <myislin@gmail.com>
2016-12-31 15:07:34 +0800

    remove plot in mydog.m

--- mydog.m
+++ mydog.m
@@ -1,8 +1,23 @@
-function dogImg = mydog(grayImg)
+function dogImg = mydog(img,sigma1,k)
 
-k = 10;
-sigma1 =  0.5;
-sigma2 = sigma1*k;
+[x y z] = size(img);
+if z > 1
+	grayImg = rgb2gray(img);
+else
+	grayImg = img;
+end
+
+switch nargin
+	case 1
+		k = 10;
+		sigma1 =  0.5;
+		sigma2 = sigma1*k;
+	case 2
+		k = 10;
+		sigma2 = sigma1*k;
+	case 3
+		sigma2 = sigma1*k;
+end
 
 hsize = [3,3];
 
@@ -13,10 +28,10 @@ gauss1 = imfilter(grayImg,h1,'replicate');
 gauss2 = imfilter(grayImg,h2,'replicate');
 
 dogImg = im2double(gauss1 - gauss2);
-figure;
-subplot(121)
-imshow(grayImg);
-subplot(122)
-imshow(dogImg);
 
+% figure;
+% subplot(121)
+% imshow(grayImg);
+% subplot(122)
+% imshow(dogImg);
 end
